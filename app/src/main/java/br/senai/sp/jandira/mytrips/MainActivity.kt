4a59a43667af84cd91670b0b89ee673c1a52638b
THAYNÁ DE OLIVEIRA FREIRE <24122484@dsn127m1247444.cfp127aluno.local>
2025-02-27 10:38:27 -0300

    configuracoes basicas e strings ok

---
+++ app/src/main/java/br/senai/sp/jandira/mytrips/MainActivity.kt
@@ -0,0 +1,28 @@
+package br.senai.sp.jandira.mytrips
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.senai.sp.jandira.mytrips.screens.LoginScreen
+import br.senai.sp.jandira.mytrips.ui.theme.MyTripsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyTripsTheme {
+                LoginScreen()
+                }
+            }
+        }
+    }
+
---
+++ app/src/main/java/br/senai/sp/jandira/mytrips/screens/LoginScreen.kt
@@ -0,0 +1,18 @@
+package br.senai.sp.jandira.mytrips.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun LoginScreen(modifier: Modifier = Modifier) {
+    Box(
+
+    )
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun LoginScreenPreview(){
+    LoginScreen()
+}
